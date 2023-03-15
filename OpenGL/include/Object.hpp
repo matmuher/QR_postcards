f3568ec16cae579434015c3b4838228d69bef013
kseniadmitrievnadobrovolskaya <kseniadmitrievnadobrovolskaya@mail.ru>
2023-03-15 15:53:42 +0300

    Graphics: add class Gift_OpenGL

--- OpenGL/include/Object.hpp
+++ OpenGL/include/Object.hpp
@@ -5,7 +5,9 @@ enum Object_type
 {
 	PINE,
 	STAR,
+	GIFT,
 };
+
 
 enum Color
 {
@@ -13,43 +15,57 @@ enum Color
 	RED,
 	VIOLET,
 	BLUE,
+	YELLOW,
 };
+
 
 class Object
 {
 
 protected:
 
-	int x_, y_;
+	float x_, y_;
 	Object_type type_;
 	Color color_;
 
 public:
 
-	Object(int x, int y, Object_type type, Color color) : x_(x), y_(y), type_(type), color_(color) {};
+	Object(float x, float y, Object_type type, Color color) : x_(x), y_(y), type_(type), color_(color) {};
 	Object() : Object(0, 0, Object_type::PINE, Color::BLUE) {};
 	Object(Object &obj) : Object(obj.x(), obj.y(), obj.type(), obj.color()) {};
 	virtual ~Object() = default;
 
-	virtual int x() const { return x_; }
-	virtual int y() const { return y_; }
+	virtual float x() const { return x_; }
+	virtual float y() const { return y_; }
 	virtual Object_type type() const { return type_; }
 	virtual Color color() const { return color_; }
 
 };
 
+
 class Pine : virtual public Object
 {
 public:
-	Pine(int x, int y, Color color = Color::BLUE) : Object(x, y, Object_type::PINE, color) {};
+	Pine(float x, float y, Color color = Color::BLUE) : Object(x, y, Object_type::PINE, color) {};
 	Pine(Object &obj) : Object(obj) {};
 	Pine(const Pine& pine) : Object(pine.x(), pine.y(), Object_type::PINE, Color::BLUE) {};
 };
 
+
 class Star : virtual public Object
 {
 public:
-	Star(int x, int y, Color color = Color::WHITE) : Object(x, y, Object_type::STAR, color) {};
+	Star(float x, float y, Color color = Color::WHITE) : Object(x, y, Object_type::STAR, color) {};
 	Star(Object &obj) : Object(obj) {};
 	Star(const Star& star) : Object(star.x(), star.y(), Object_type::STAR, Color::WHITE) {};
 };
+
+
+class Gift : virtual public Object
+{
+public:
+	Gift(float x, float y, Color color = Color::BLUE) : Object(x, y, Object_type::GIFT, color) {};
+	Gift(Object &obj) : Object(obj) {};
+	Gift(const Gift& gift) : Object(gift.x(), gift.y(), Object_type::GIFT, Color::BLUE) {};
+};
+
